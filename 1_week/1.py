d45c9162e63a8b0d2e64bd3b435f80499d77a871
Roman Kachko <kachkoroman2005@gmail.com>
2026-03-25 09:48:35 +0500

    fix 1 task

--- 1_week/1.py
+++ 1_week/1.py
@@ -1,1 +1,17 @@
-    
+value = int(input("Enter value: "))
+power = int(input("Enter power: "))
+
+
+def calculate_power(value, power):
+    if power == 0:
+        return 1
+    elif power % 2 == 0:
+        return calculate_power(value, power // 2) * calculate_power(value, power // 2)
+    else:
+        return calculate_power(value, (power - 1) // 2) * calculate_power(value, (power - 1) // 2) * value   
+
+if __name__ == "__main__":
+
+    result = calculate_power(value=value, power=power)
+
+    print(f"Result: {result}") 
